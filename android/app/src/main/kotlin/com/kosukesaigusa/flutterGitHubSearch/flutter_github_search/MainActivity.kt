afcb1059604133b60d0b4890c6db6de4fc9d04ca
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-04-16 15:01:36 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/kosukesaigusa/flutterGitHubSearch/flutter_github_search/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kosukesaigusa.flutterGitHubSearch.flutter_github_search
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_github_search/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
