c9050201518f2344a864c68b539350eff841b5c7
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-04-27 19:43:12 +0900

    docs: fix comment

--- lib/models/api_response/api_result/api_result.dart
+++ lib/models/api_response/api_result/api_result.dart
@@ -2,8 +2,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'api_result.freezed.dart';
 
-/// 借字で実装した。
-/// 用途・必要性の検討中。
+/// 仮で実装した。用途・必要性の検討中。
 @freezed
 class ApiResult with _$ApiResult {
   const factory ApiResult.success({
