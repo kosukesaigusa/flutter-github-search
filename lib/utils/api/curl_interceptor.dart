2f9cbe4c096b9dfb0984968a3cec5f9e6c2b467e
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-05-01 13:28:55 +0900

    refactor: refactor curl interceptor

--- lib/utils/api/curl_interceptor.dart
+++ lib/utils/api/curl_interceptor.dart
@@ -10,23 +10,24 @@ class CurlInterceptor extends Interceptor {
     RequestOptions options,
     RequestInterceptorHandler handler,
   ) async {
-    debugPrint('*** curl ***');
-    final curl = StringBuffer();
+    debugPrint('*** Request ***');
+    final stringBuffer = StringBuffer();
     var query = '';
     if (options.method == 'GET' && options.queryParameters.isNotEmpty) {
       query =
           '?${options.queryParameters.entries.map((e) => '${e.key}=${e.value}').toList().join('&')}';
     }
     final requestUrl = '${options.baseUrl}${options.path}$query';
-    curl.write('curl --request ${options.method} \'$requestUrl\'');
+    stringBuffer.write('curl --request ${options.method} \'$requestUrl\'');
     for (final key in options.headers.keys) {
-      curl.write(' -H \'$key: ${options.headers[key]}\'');
+      stringBuffer.write(' -H \'$key: ${options.headers[key]}\'');
     }
     if (options.data != null && options.data is Map) {
-      curl.write(' --data-binary \'${jsonEncode(options.data)}\'');
+      stringBuffer.write(' --data-binary \'${jsonEncode(options.data)}\'');
     }
-    curl.write(' --insecure');
-    debugPrint(curl.toString());
-    return handler.next(options);
+    // SSL 証明書のエラーを無視する
+    stringBuffer.write(' --insecure');
+    debugPrint(stringBuffer.toString());
+    super.onRequest(options, handler);
   }
 }
