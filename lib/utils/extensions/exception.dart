16c33334d5f60f6365af10745ed2e9a242430765
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-05-07 16:52:24 +0900

    refactor: update snackbar exception message

---
+++ lib/utils/extensions/exception.dart
@@ -0,0 +1,13 @@
+import '../../constants/string.dart';
+
+extension ExceptionExtension on Exception {
+  /// SnackBar などで表示して差し支えのないメッセージを取得する
+  String get displayMessage {
+    final message = toString()
+        .trimLeft()
+        .trimRight()
+        .replaceAll('Exception: ', '')
+        .replaceAll(RegExp(r'^Exception$'), '');
+    return message.isEmpty ? generalExceptionMessage : message;
+  }
+}
