2a1ffa9963780a723f20c0528493188d9711c4e3
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-05-07 21:56:28 +0900

    docs: update comments

--- lib/utils/exceptions/base.dart
+++ lib/utils/exceptions/base.dart
@@ -6,13 +6,13 @@ class AppException implements Exception {
     this.defaultMessage = 'エラーが発生しました。',
   });
 
-  ///
+  /// ステータスコードや独自のエラーコードなどのエラー種別を識別するための文字列
   final String? code;
 
-  ///
+  /// 例外の内容を説明するメッセージ
   final String? message;
 
-  ///
+  /// message が空の場合に使用されるメッセージ
   final String defaultMessage;
 
   @override
