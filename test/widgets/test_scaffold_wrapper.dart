a7c29cf2eb7c66317bd6d124e28f9a2ab54253f8
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-05-09 21:29:57 +0900

    feat: enable test scaffold wrapper widget to have riverpod overrides

--- test/widgets/test_scaffold_wrapper.dart
+++ test/widgets/test_scaffold_wrapper.dart
@@ -6,14 +6,17 @@ class TestScaffoldWrapper extends StatelessWidget {
   const TestScaffoldWrapper({
     Key? key,
     required this.child,
+    this.overrides = const <Override>[],
   }) : super(key: key);
 
   final Widget child;
+  final List<Override> overrides;
 
   @override
   Widget build(BuildContext context) {
     return RootWidget(
       child: ProviderScope(
+        overrides: overrides,
         child: MaterialApp(
           home: ScaffoldMessenger(
             child: Scaffold(
